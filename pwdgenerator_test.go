c4df45a62e9ad9ce667f350d59d6b25f86a19c5e
Alex <53578660+Flussen@users.noreply.github.com>
2024-02-17 19:21:57 +0100

    improvements in the pwdgenerator_test

--- pwdgenerator_test.go
+++ pwdgenerator_test.go
@@ -1,23 +1,46 @@
 package main
 
 import (
-	"fmt"     // Package for formatted I/O
-	"log"     // Package for logging
+	// Package for formatted I/O
+	"log" // Package for logging
+	"strings"
 	"testing" // Package for testing
 
 	"github.com/stretchr/testify/assert" // Package for assertions in tests
 )
 
-// TestPassword is a test function for PasswordGenerator
-func TestPassword(t *testing.T) {
+// # TestPassword is a test function for PasswordGenerator.
+//
+// Contains the password generation test and receiving the status,
+// also test the length of the password,
+// and check if the password has any of the requested special characters.
+func Test_password_if_the_password_is_ok(t *testing.T) {
 	length := 20 // Length of the password to generate
+	specialCharacters := "!@#$%^&*()-_+=[]{};:,.<>?/|"
+	containsSpecialCharacters := false
 
 	// Generate a password and get its strength status
 	pwd, status := NewApp().PasswordGenerator(length)
 
-	// Assert that the generated password and status are not nil
+	// Assert notnil that the generated password and status are not nil
 	if assert.NotNil(t, pwd, status) {
-		log.Println("Password generated, not nil")
-		fmt.Print(pwd, status) // Print the generated password and its status
+		log.Printf("TEST GENERATED: Password generated successfully, not null.\nPassword: %s\nstatus: %s", pwd, status)
+	}
+	// Assert len that if the length is 20
+	if assert.Len(t, pwd, 20) {
+		log.Printf("TEST LENGTH: Length is %v exactly", length)
+	}
+
+	// Assert true if the password contains at least one special character.
+	for _, char := range specialCharacters {
+		if strings.ContainsRune(pwd, char) {
+			containsSpecialCharacters = true
+			break
+		}
+	}
+	if assert.True(t, containsSpecialCharacters) {
+		log.Printf("TEST CONTAINS: The password contains at least one special character.")
+	} else {
+		log.Fatal("The password does not contain any special characters.")
 	}
 }
