07b37907d8224f8a3353f0d3f36538e4d42eef82
Flussen <53578660+Flussen@users.noreply.github.com>
2024-02-23 20:55:55 +0100

    register fixed, register test added, better UserKey generator

---
+++ app/database_test.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"os"
+	"testing"
+
+	"go.etcd.io/bbolt"
+)
+
+func createTestDB(t *testing.T) (*bbolt.DB, func()) {
+	// Crear un archivo temporal para la base de datos
+	tmpfile, err := os.CreateTemp("", "testdb_*.db")
+	if err != nil {
+		t.Fatalf("failed to create temp file: %v", err)
+	}
+
+	// Abrir la base de datos bbolt en el archivo temporal
+	db, err := bbolt.Open(tmpfile.Name(), 0600, nil)
+	if err != nil {
+		t.Fatalf("failed to open db: %v", err)
+	}
+
+	// Función para cerrar y eliminar la base de datos temporal
+	cleanup := func() {
+		db.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return db, cleanup
+}
