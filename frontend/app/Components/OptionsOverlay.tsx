9fd1de8b5458a11f9f10dcc00f0079c9061258d4
Busta <josuebustatr@gmail.com>
2024-03-17 14:30:25 +0100

    Icons added

--- frontend/app/Components/OptionsOverlay.tsx
+++ frontend/app/Components/OptionsOverlay.tsx
@@ -12,53 +12,93 @@ import GridViewRoundedIcon from '@mui/icons-material/GridViewRounded';
 import PasswordRoundedIcon from '@mui/icons-material/PasswordRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
 import ExitToAppRoundedIcon from '@mui/icons-material/ExitToAppRounded';
+import foursquare from "../../Public/foursquare.svg";
+import gridsquares from "../../Public/grid_view.svg";
+import { DoLogout } from "@/wailsjs/wailsjs/go/app/App";
+import DashboardIconColor from "../../Public/grid_view.svg"
+import PasswordIconColor from "../../Public/password.svg"
+import FolderCopyRoundedIcon from '@mui/icons-material/FolderCopyRounded';
+
 interface OptionsOverlayProps {
   setShowGenerator: (show: boolean) => void;
-showGenerator: boolean;
+  showGenerator: boolean;
   children: React.ReactNode;
   userName: string;
+  setShowDashboard:(show: boolean)=> void;
 }
 
-const OptionsOverlay: React.FC<OptionsOverlayProps> = ({ setShowGenerator, showGenerator, children, userName }) => {
+const OptionsOverlay: React.FC<OptionsOverlayProps> = ({setShowDashboard,  setShowGenerator, showGenerator, children, userName }) => {
 
+  const[showSecureFiles, setShowSecureFiles]= useState(false);
+  
+
+  async function Logout(){
+    try{
+
+      const result = await DoLogout(userName);
+      setShowDashboard(false);
+      console.log(userName)
+      console.log('Logout'+result)
+    }catch{
+      alert('error al logout')
+    }
+  }
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    await Logout(); 
+
+
+  };
 
   return (
 
 
-    <div id="Options" className="fixed flex flex-col justify-between 2xl:w-[20%] w-[100px] h-screen bg-box rounded-tr-2xl rounded-br-2xl shadow-shadow pt-20">
-      <div className="flex-col justify-center w-full  2xl:px-[15%]  px-[20px]  space-y-16">
+    <div id="Options" className="fixed flex flex-col justify-between 2xl:w-[20%] w-[100px]  bg-blackbox border-2 border-border rounded-lg  shadow-shadow ml-8 mt-8 mb-8 pt-[4.5rem]">
+      <div className="flex-col justify-center w-full  2xl:px-[15%]  px-[20px]  space-y-[3rem]">
 
 
-        <div className='flex justify-center text-blue font-bold 2xl:text-5xl text-md'>
-          Go<span className='text-bkblue'>Pass</span>
+        <div className='flex justify-center text-back font-bold 2xl:text-5xl text-md'>
+          <span className='bg-gradient bg-clip-text text-transparent'>Go</span><span >Pass</span>
         </div>
-        <div className="flex justify-center items-center text-xl space-x-2 ">
+        <div className="flex justify-center items-center text-xl space-x-2 text-back ">
           <AccountCircleRoundedIcon sx={{ fontSize: 50 }} />
-          <div className=" 2xl:flex hidden text-grey font-semibold">
+          <div className=" 2xl:flex hidden text-darkgrey font-semibold">
             {userName}
           </div>
         </div>
         <div className="space-y-2">
-          <div onClick={() => { setShowGenerator(false) }} className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl space-x-2 cursor-pointer  font-semibold 2xl:text-xl  ${!showGenerator ? 'bg-bgblue text-blue' : 'text-grey  hover:text-blue'}`}>
-            <GridViewRoundedIcon sx={{ fontSize: 28 }} />
-            <div className=" 2xl:flex hidden">My Passwords</div>
+          <div onClick={() => { setShowGenerator(false) }} className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl  space-x-2 cursor-pointer  font-semibold 2xl:text-xl  ${!showGenerator ? ' text-back max-2xl:bg-border' : 'text-darkgrey  hover:text-back'}`}>
+          {!showGenerator?
+          <Image src={DashboardIconColor} alt="dahsborad_icon_color"/>:
+          <GridViewRoundedIcon sx={{ fontSize: 25}}/>
+        }
+            <div className={`2xl:flex hidden `}>My Passwords</div>
           </div>
-          <div onClick={() => { setShowGenerator(true) }} className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl space-x-2 cursor-pointer 2xl:text-xl xl:text-base  font-semibold ${showGenerator ? 'bg-bgblue text-blue' : 'text-grey  hover:text-blue'}`}>
-            <PasswordRoundedIcon sx={{ fontSize: 28 }} />
+          <div onClick={() => { setShowGenerator(true) }} className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl space-x-2 cursor-pointer 2xl:text-xl xl:text-base  font-semibold ${showGenerator ? 'text-back max-2xl:bg-border' : 'text-darkgrey  hover:text-back'}`}>
+            {showGenerator?
+            <Image src={PasswordIconColor} alt="passwor_icon_color"/>:
+            <PasswordRoundedIcon sx={{ fontSize: 25 }} />
+
+          }
             <div className=" 2xl:flex hidden">Pass Generator</div>
+          </div>
+          <div onClick={() => { setShowSecureFiles(false) }} className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl space-x-2 cursor-pointer 2xl:text-xl xl:text-base  font-semibold ${showSecureFiles ? ' text-back max-2xl:bg-border' : 'text-darkgrey  hover:text-back'}`}>
+            <FolderCopyRoundedIcon sx={{ fontSize: 25 }} />
+            <div className=" 2xl:flex hidden">Secure Files</div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col justify-between w-full 2xl:px-[15%]  px-[20px]  space-y-2 mb-10">
-        <div className="flex items-center 2xl:justify-start justify-center 2xl:pl-[10%]  h-14 rounded-2xl  hover:text-blue space-x-2 cursor-pointer text-xl font-semibold text-grey" >
+      <div className="flex flex-col justify-between w-full 2xl:px-[15%]  px-[20px]  space-y-2 mt-[5.5rem] mb-5 ">
+        <div className="flex items-center 2xl:justify-start justify-center 2xl:pl-[10%]  h-14 rounded-2xl  hover:text-back space-x-2 cursor-pointer text-xl font-semibold text-darkgrey" >
           <SettingsRoundedIcon />
           <div className=" 2xl:flex hidden">
             Settings
           </div>
         </div>
-        <div className="flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl  hover:text-red space-x-2 cursor-pointer text-xl font-semibold text-grey">
+        <div onClick={handleSubmit} className="flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl  hover:text-red space-x-2 cursor-pointer text-xl font-semibold text-darkgrey">
           <ExitToAppRoundedIcon />
-          <div className=" 2xl:flex hidden">
+          <div  className=" 2xl:flex hidden">
             Log Out
           </div>
         </div>
--- frontend/app/Components/PasswordComp.tsx
+++ frontend/app/Components/PasswordComp.tsx
@@ -5,6 +5,9 @@ import { ShowPassword } from "@/wailsjs/wailsjs/go/app/App";
 import GppMaybeRoundedIcon from '@mui/icons-material/GppMaybeRounded';
 import ShieldRoundedIcon from '@mui/icons-material/ShieldRounded';
 import EditOverlay from './EditOverlay';
+import Tooltip from '@mui/material/Tooltip';
+import { Instance } from '@popperjs/core';
+import Box from '@mui/material/Box';
 
 
 interface PassProps {
@@ -12,6 +15,9 @@ interface PassProps {
     userKey: string;
     isAddOverlayOpen: boolean;
     search: string;
+    setArePasswords:(pass:boolean) => void;
+    arePasswords:boolean;
+    setIsAddOverlayOpen: (show: boolean) =>void;
 }
 
 interface PasswordsProps {
@@ -23,17 +29,35 @@ interface PasswordsProps {
 }
 
 
-const PasswordComp: React.FC<PassProps> = ({ userName, userKey, isAddOverlayOpen, search }) => {
+const PasswordComp: React.FC<PassProps> = ({ userName, userKey, isAddOverlayOpen, search, setArePasswords, arePasswords, setIsAddOverlayOpen }) => {
     const [passwords, setPasswords] = useState<PasswordsProps[]>([]);
     const [decrypted, setDecrypted] = useState('');
     const [isEditOverlayOpen, setIsEditOverlayOpen] = useState(false);
     const [openEditOverlayId, setOpenEditOverlayId] = useState<string | null>(null);
-
-
+    const [iconSelection, setIconSeleciotn] = useState('');
     useEffect(() => {
         getPasswords();
     }, [openEditOverlayId, isAddOverlayOpen]);
 
+    useEffect(() => {
+        console.log('OpenEdit: ' + openEditOverlayId);
+    }, []);
+
+
+    const positionRef = React.useRef<{ x: number; y: number }>({
+        x: 0,
+        y: 0,
+    });
+    const popperRef = React.useRef<Instance>(null);
+    const areaRef = React.useRef<HTMLDivElement>(null);
+
+    const handleMouseMove = (event: React.MouseEvent) => {
+        positionRef.current = { x: event.clientX, y: event.clientY };
+
+        if (popperRef.current != null) {
+            popperRef.current.update();
+        }
+    };
 
 
 
@@ -45,12 +69,13 @@ const PasswordComp: React.FC<PassProps> = ({ userName, userKey, isAddOverlayOpen
                 const decryptedPasswords = await Promise.all(data.passwords.map(async (password: PasswordsProps) => {
                     const decryptedPwd = await ShowPassword(userName, password.id, userKey);
                     return { ...password, pwd: decryptedPwd };
-
+                   
                 }));
+                setArePasswords(true)
                 setPasswords(decryptedPasswords);
-                console.log(data.passwords)
             } else {
                 console.error("Passwords not found in response:", data);
+                setArePasswords(false)
             }
         } catch (error) {
             console.error("Error fetching passwords:", error, userName);
@@ -67,55 +92,129 @@ const PasswordComp: React.FC<PassProps> = ({ userName, userKey, isAddOverlayOpen
         }
     };
 
-const searchPasswords = passwords.filter((password) => password.title.toLowerCase().includes(search.toLowerCase()));
+    const searchPasswords = passwords.filter((password) => password.title.toLowerCase().includes(search.toLowerCase()));
+   
 
-return (
-    <div className="flex-col w-full space-y-5">
-        {searchPasswords.map((password, index) => (
+    return (
+        <>
+        {
+            arePasswords?
+            <div className="flex-col w-full space-y-5">
+            {searchPasswords.map((password, index) => (
 
-            <div key={index} className="w-full ">
-                <div onClick={() => setOpenEditOverlayId(openEditOverlayId === password.id ? null : password.id)} className="flex w-full h-24 bg-box p-3 rounded-2xl text-xl">
-                    <div className="flex items-center basis-3/6 space-x-5">
-                        <div className="rounded-xl bg-white w-[4.5rem] h-full flex items-center justify-center">
-                            G
-                        </div>
-                        <div className="flex-col text-md">
-                            <div className="font-bold">
-                                {password.title}
+                <div key={index} className="w-full ">
+                    <div onClick={() => setOpenEditOverlayId(openEditOverlayId === password.id ? null : password.id)} className="flex w-full h-24 bg-blackbox border-2 border-border p-3 rounded-lg text-xl cursor-pointer">
+                        <div className="flex items-center basis-3/6 space-x-5">
+                            <div className="rounded-lg bg-black border-2 border-border text-white w-[4.5rem] h-full flex items-center justify-center">
+                                G
                             </div>
-                            <div className="text-grey text-lg">
-                                {password.username}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex items-center basis-2/6">
-                        <input onClick={() => copyToClipboard(password.pwd)} readOnly type="password" value={password.pwd.length > 15 ? `${password.pwd.substring(0, 15)}` : password.pwd} maxLength={20} className="bg-transparent focus:outline-none cursor-pointer" />
-
-                    </div>
-                    <div className="flex items-center  xl:basis-1/6">
-
-                        <div className={`flex items-center justify-center rounded-full h-10 xl:w-40 max-xl:px-2 ${password.pwd.length > 25 ? 'bg-lightgreen  text-green' : password.pwd.length > 10 ? 'bg-red-300  text-orange-500 bg-orange-100' : 'text-red bg-rose-100'} `}>
-                            {password.pwd.length > 25 ? <GppGoodRoundedIcon /> : password.pwd.length > 10 ? <ShieldRoundedIcon /> : <GppMaybeRoundedIcon />
-
-
-                            }
-
-                            <div className="hidden xl:flex">
-                                {password.pwd.length > 25 ? 'Strong' : password.pwd.length > 10 ? 'Medium' : 'Weak'}
+                            <div className="flex-col text-md">
+                                <div className="font-bold bg-gradient bg-clip-text text-transparent inline-block">
+                                    {password.title}
+                                </div>
+                                <div className="text-darkgrey text-lg">
+                                    {password.username}
+                                </div>
                             </div>
                         </div>
+                        <div className="flex items-center basis-2/6">
+                            <Tooltip
+
+                                title="Click to copy"
+                                placement="top"
+                                arrow
+                                PopperProps={{
+                                    popperRef,
+                                    anchorEl: {
+                                        getBoundingClientRect: () => {
+                                            return new DOMRect(
+                                                positionRef.current.x,
+                                                areaRef.current!.getBoundingClientRect().y,
+                                                0,
+                                                0,
+                                            );
+                                        },
+                                    },
+                                }}
+                            >
+                                <div ref={areaRef}
+                                    onMouseMove={handleMouseMove}>
+                                    <input onClick={() => copyToClipboard(password.pwd)} readOnly type="password" value={password.pwd.length > 15 ? `${password.pwd.substring(0, 15)}` : password.pwd} maxLength={20} className=" text-back bg-transparent focus:outline-none cursor-pointer" />
+                                </div>
+
+                            </Tooltip>
+
+                        </div>
+
+
+                        <div className="flex items-center  xl:basis-1/6">
+                            <Tooltip
+
+                                title={`Password level of security is ${password.pwd.length > 25 ? 'Strong' : password.pwd.length > 10 ? 'Medium' : 'Weak'}`}
+                                placement="top"
+                                arrow
+                                PopperProps={{
+                                    popperRef,
+                                    anchorEl: {
+                                        getBoundingClientRect: () => {
+                                            return new DOMRect(
+                                                positionRef.current.x,
+                                                areaRef.current!.getBoundingClientRect().y,
+                                                0,
+                                                0,
+                                            );
+                                        },
+                                    },
+                                }}
+                            >
+
+
+
+
+
+                                <div ref={areaRef}
+                                    onMouseMove={handleMouseMove} className={`flex items-center justify-center h-10  max-xl:px-2 ${password.pwd.length > 25 ? '  text-purple' : password.pwd.length > 10 ? ' text-orange' : 'text-red '} `}>
+                                    {password.pwd.length > 25 ? <GppGoodRoundedIcon /> : password.pwd.length > 10 ? <ShieldRoundedIcon /> : <GppMaybeRoundedIcon />
+
+
+                                    }
+
+                                    <div className="hidden xl:flex">
+                                        {password.pwd.length > 25 ? 'Strong' : password.pwd.length > 10 ? 'Medium' : 'Weak'}
+                                    </div>
+                                </div>
+                            </Tooltip>
+                        </div>
                     </div>
+                    <EditOverlay isOpen={openEditOverlayId === password.id} onClose={() => setOpenEditOverlayId(null)} userNames={userName} userKey={userKey} password={password.pwd} title={password.title} username={password.username} id={password.id}>
+                        <></>
+                    </EditOverlay>
+
                 </div>
-                <EditOverlay isOpen={openEditOverlayId === password.id} onClose={() => setOpenEditOverlayId(null)} userNames={userName} userKey={userKey} password={password.pwd} title={password.title} username={password.username} id={password.id}>
-                    <></>
-                </EditOverlay>
 
+            ))
+            }
+
+        </div >
+        :
+
+        <div className="text-back w-full flex-col flex justify-center items-center space-y-7 ">
+            <div className="text-2xl">
+            Add your first password!
+            </div> 
+            <div onClick={() => setIsAddOverlayOpen(!isAddOverlayOpen)} className="bg-gradient p-0.5 rounded-lg bn5 group">
+            <button className="bg-black py-2 px-7 rounded-md group-hover:bg-transparent group-hover:text-black">
+                Add Password
+            </button>
             </div>
-
-        ))}
-
-    </div>
-);
+            
+        </div>
+        }
+        
+        </>
+        
+        
+    );
 }
 
 export default PasswordComp;
