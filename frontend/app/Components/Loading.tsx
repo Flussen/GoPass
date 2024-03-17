94f1444ee9dc6ba3bccfa138c839be13be205335
Busta <josuebustatr@gmail.com>
2024-03-17 14:29:33 +0100

    New loading animation and design

--- frontend/app/Components/Loading.tsx
+++ frontend/app/Components/Loading.tsx
@@ -8,10 +8,10 @@ const LoadingComp=({}) => {
 
 
   return (
-    <div className='flex  items-center justify-center h-screen space-x-2 bg-back' id='loading'>
-      <div className='bg-blue rounded-full h-2 w-2 animate-loading '/>
-      <div className='bg-blue rounded-full h-2 w-2 animate-loading delay-500'/>
-      <div className='bg-blue rounded-full h-2 w-2 animate-loading delay-1000'/>
+    <div className='flex  items-center justify-center h-screen space-x-2 bg-black' id='loading'>
+      <div className='bg-gradient  w-2 animate-loading '/>
+      <div className='bg-gradient w-2 animate-loading delay-500'/>
+      <div className='bg-gradient w-2 animate-loading delay-1000'/>
 
     </div>
   );
