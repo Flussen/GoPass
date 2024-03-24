d4f17c145ef2c0e1e0d797d41718c653d347066c
Flussen <53578660+Flussen@users.noreply.github.com>
2024-03-24 18:26:24 +0100

    to main, first release

--- backend/sessiontoken/get_key.go
+++ backend/sessiontoken/get_key.go
@@ -1,15 +1,5 @@
 package sessiontoken
 
-import (
-	"os"
-
-	"github.com/joho/godotenv"
-)
-
 func getKey() (string, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return "", err
-	}
-	return os.Getenv("SECRETKEY"), nil
+	return "YOURSECRETKEY", nil
 }
