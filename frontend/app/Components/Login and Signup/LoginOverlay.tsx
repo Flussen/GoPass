9bd408accd158127a1b8c7f632d54550223934d2
Busta <josuebustatr@gmail.com>
2024-04-06 14:42:10 +0200

    x from login now works

--- frontend/app/Components/Login and Signup/LoginOverlay.tsx
+++ frontend/app/Components/Login and Signup/LoginOverlay.tsx
@@ -13,7 +13,7 @@ interface RegisterProps {
     isOpen: boolean;
     onClose: () => void;
     children: React.ReactNode;
-    setShowRecover: (show:boolean) => void;
+    setShowRecover: (show: boolean) => void;
     setUserKey: (userKey: string) => void;
     setUserName: (userKey: string) => void;
     setShowDashboard: (show: boolean) => void;
@@ -29,35 +29,35 @@ const LoginOverlay: React.FC<RegisterProps> = ({ isOpen, onClose, children, setS
     const LoginData = new request.Login({
         account: name,
         password: password
-      })
-    
+    })
+
     async function pullLogin(LoginData: request.Login) {
 
         setLoadingIsOpen(true)
         try {
-          const result = await DoLogin(LoginData);
-          console.log(result)
-          if (result.token !== null && result.token !== '' && result.userKey !== null && result.userKey !== '') {
-            setUserKey(result.userKey);
-            setShowDashboard(true);
-            console.log('Token Saved:' + result.token)
-            setUserName(name)
-          }
+            const result = await DoLogin(LoginData);
+            console.log(result)
+            if (result.token !== null && result.token !== '' && result.userKey !== null && result.userKey !== '') {
+                setUserKey(result.userKey);
+                setShowDashboard(true);
+                console.log('Token Saved:' + result.token)
+                setUserName(name)
+            }
         } catch (error) {
-          console.log(error)
-          setPasswordIncorrect(true)
+            console.log(error)
+            setPasswordIncorrect(true)
         } finally {
-          setLoadingIsOpen(false)
+            setLoadingIsOpen(false)
         }
-      }
-    
-    
-      const handleSubmit = async (event: React.FormEvent) => {
+    }
+
+
+    const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault(); // Previene la recarga de la página
         await pullLogin(LoginData); // Llama a la función pullLogin
         // Simula una carga o espera por una operación asíncrona
-    
-      };
+
+    };
 
     return (
         <>
@@ -67,8 +67,8 @@ const LoginOverlay: React.FC<RegisterProps> = ({ isOpen, onClose, children, setS
                         <div onClick={onClose} className='absolute bg-[#000000] opacity-80 h-screen w-screen '></div>
                         <div className='flex flex-col justify-center bg-darkgray  w-[57rem] h-[75%] rounded-lg z-10 text-whitebg  font-semibold'>
                             <div className="  text-whitebg w-full flex justify-end items-start  pr-5 pt-5">
-                                <div onClick={onClose}>
-                                <CloseRoundedIcon className="cursor-pointer" />
+                                <div onClick={onClose} className="cursor-pointer z-20">
+                                    <CloseRoundedIcon className="cursor-pointer" />
 
                                 </div>
                             </div>
@@ -81,10 +81,10 @@ const LoginOverlay: React.FC<RegisterProps> = ({ isOpen, onClose, children, setS
                                     <input type="text" className="bg-black rounded-full h-12 w-full pl-12 outline-none placeholder:text-gray " placeholder="Username" value={name} onChange={(e) => setName(e.target.value)} />
 
                                 </div>
-                                
+
                                 <div className="w-full flex items-center">
                                     <KeyRoundedIcon className="absolute ml-4 text-primary" />
-                                    <input type="password" className="bg-black rounded-full h-12 w-full pl-12 outline-none placeholder:text-gray " placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                    <input type="password" className="bg-black rounded-full h-12 w-full pl-12 outline-none placeholder:text-gray " placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
                                 </div>
                                 <div onClick={handleSubmit} className="w-full">
@@ -93,14 +93,14 @@ const LoginOverlay: React.FC<RegisterProps> = ({ isOpen, onClose, children, setS
                                     </button>
                                 </div>
                                 <div className="w-full flex justify-center text-gray font-medium">
-                                    Forget the password?&nbsp; <span onClick={()=>setShowRecover(true)} className="text-primary font-semibold cursor-pointer">Recover</span>
+                                    Forget the password?&nbsp; <span onClick={() => setShowRecover(true)} className="text-primary font-semibold cursor-pointer">Recover</span>
                                 </div>
                             </div>
- 
+
                         </div>
                         {loadingIsOpen ?
-            <LoadingComp /> :
-            <></>}
+                            <LoadingComp /> :
+                            <></>}
                     </div>
                 )
                     : null
