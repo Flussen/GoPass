cf02ed8ae8a2779f50bed79342fccaad2a695568
Busta <josuebustatr@gmail.com>
2024-04-06 22:59:10 +0200

    New settings options  overlay

--- frontend/app/Components/Settings/SettingOverlay.tsx
+++ frontend/app/Components/Settings/SettingOverlay.tsx
@@ -4,42 +4,60 @@ import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import MonitorRoundedIcon from '@mui/icons-material/MonitorRounded';
 import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
 import ManageAccountsRoundedIcon from '@mui/icons-material/ManageAccountsRounded';
+
 interface SettingsProps {
     setShowProfile: (show: boolean) => void;
+    setOptionName: (show: string) => void;
+    optionName:string;
 }
 
-const SettingsComp: React.FC<SettingsProps> = ({ setShowProfile }) => {
+const SettingsComp: React.FC<SettingsProps> = ({ setShowProfile, setOptionName, optionName }) => {
 
     return (
-        <div id="Options" className="fixed flex flex-col justify-between 2xl:w-[20%] w-[100px]  bg-darkgray  rounded-lg   ml-8 mt-8 mb-8 pt-[0.5rem] h-[92%] ">
-            <div className="flex-col justify-center w-full   px-[20px]  space-y-[3rem]">
-                <div onClick={() => { setShowProfile(false) }} className="flex items-center justify-center h-14 w-14 mt-5 2xl:ml-2  rounded-lg bg-gray cursor-pointer text-primary hover:bg-whitegray">
-                    <ArrowBackIosNewRoundedIcon sx={{ fontSize: 28 }}/>
-                </div>
+        <div id="Options" className="fixed flex flex-col justify-between xl:w-[16%] w-[75px]  bg-darkgray  pb-12   h-full ">
+        <div className="flex-col  w-full  ">
+  
+  
+          <div onClick={()=>setShowProfile(false)} className='flex justify-center items-center bg-gray text-whitegray hover:text-whitebg cursor-pointer h-12 w-12 m-3 pr-1 rounded-lg  '>
+          <ArrowBackIosNewRoundedIcon sx={{ fontSize: 24 }} />
 
-                
-                <div className="space-y-2">
-                    <div className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl  space-x-2 cursor-pointer  font-semibold 2xl:text-xl  ${true ? ' text-primary max-2xl:bg-gray' : 'text-gray  hover:text-whitegray'}`}>
-
-                    <ManageAccountsRoundedIcon sx={{ fontSize: 28 }} className="" />
-
-                        <div className={`2xl:flex hidden `}>Your Account</div>
-                    </div>
-                    <div className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl space-x-2 cursor-pointer 2xl:text-xl xl:text-base  font-semibold ${false ? 'text-primary max-2xl:bg-gray' : 'text-whitegray  hover:text-primary'}`}>
-
-                    <MonitorRoundedIcon sx={{ fontSize: 28 }} className="" />
-
-                        <div className=" 2xl:flex hidden">Apparence</div>
-                    </div>
-                    <div className={`flex items-center 2xl:justify-start justify-center 2xl:pl-[10%] h-14 rounded-2xl space-x-2 cursor-pointer 2xl:text-xl xl:text-base  font-semibold ${false ? ' text-primary max-2xl:bg-border' : 'text-whitegray  hover:text-primary'}`}>
-                    <HelpRoundedIcon sx={{ fontSize: 28 }} className="" />
-                        <div className=" 2xl:flex hidden">Help</div>
-                    </div>
-                </div>
+          </div>
+  
+          <div className="">
+           
+            <div onClick={() => { setOptionName('') }} className={`flex items-center xl:justify-start justify-center w-full  border-l-2 h-16 space-x-2 cursor-pointer text-whitegray  font-semibold xl:text-xl xl:pl-5 ${optionName == '' ? 'bg-black border-primary' : 'hover:text-blackwhite border-darkgray'}`}>
+  
+              <ManageAccountsRoundedIcon sx={{ fontSize: 32 }} />
+  
+  
+              <div className=" xl:flex hidden">My Groups</div>
             </div>
-          
-
+            <div onClick={() => { setOptionName('Cards') }} className={`flex items-center xl:justify-start justify-center w-full border-l-2  h-16 space-x-2 cursor-pointer text-whitegray  font-semibold xl:text-xl xl:pl-5 ${optionName == 'Cards' ? 'bg-black  border-primary' : 'hover:text-blackwhite border-darkgray '}`}>
+  
+              <MonitorRoundedIcon sx={{ fontSize: 32 }} />
+  
+  
+              <div className=" xl:flex hidden">My Cards</div>
+            </div>
+            <div onClick={() => { setOptionName('Generator') }} className={`flex items-center xl:justify-start justify-center w-full   border-l-2 h-16 space-x-2 cursor-pointer text-whitegray  font-semibold xl:text-xl xl:pl-5 ${optionName == 'Generator' ? 'bg-black border-l-2 border-primary' : 'hover:text-blackwhite border-darkgray'}`}>
+  
+              <HelpRoundedIcon sx={{ fontSize: 32 }} />
+  
+  
+              <div className=" xl:flex hidden"> <span className="2xl:flex hidden">Pass&nbsp;</span>Generator</div>
+            </div>
+            
+          </div>
         </div>
+        <div className="flex flex-col justify-between w-full      mt-[5.5rem]  ">
+          
+          <div className={`flex items-center xl:justify-start justify-center w-full  border-l-2  h-16 space-x-2 cursor-pointer text-whitegray  font-semibold xl:text-xl xl:pl-5 hover:text-whitebg  border-darkgray`}>
+            <AccountCircleRoundedIcon sx={{ fontSize: 32 }} />
+            <div className=" xl:flex hidden">Log Out</div>
+          </div>
+        </div>
+  
+      </div>
     )
 }
 
